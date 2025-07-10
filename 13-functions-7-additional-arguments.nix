962a4e861b97f7bc683dcd59191aca0d84f3c68c
Hung Vu <hung.vu@nokia.com>
2025-07-10 11:55:02 +0300

    fix: Change falsely named files (function-7 and function-8 were in reversed order)

--- 13-functions-7-additional-arguments.nix
+++ 13-functions-7-additional-arguments.nix
@@ -1,4 +1,6 @@
 let
-  f = {a, b, ...}@args: a + b + args.c;
+  f = {a, b, ...}: a + b;
 in
 f { a = 1; b = 2; c = 3; }
+
+# Additional arguments can be allowed with the "..." syntax.
--- 13-functions-8-named-argument-set.nix
+++ 13-functions-8-named-argument-set.nix
@@ -1,6 +1,10 @@
 let
-  f = {a, b, ...}: a + b;
+  f = {a, b, ...}@args: a + b + args.c;
+  g = args@{a, b, ...}: a + b + args.c;
 in
-f { a = 1; b = 2; c = 3; }
+[
+  f { a = 1; b = 2; c = 3; }
+]
 
-# Additional arguments can be allowed with the "..." syntax.
+# It is possible to assign a name to the attribute set by using the '@' character.
+# In the above example, 'f' and 'g' are equivalent ways of assigning name to the argument set.
